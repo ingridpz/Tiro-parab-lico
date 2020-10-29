b418b72c7f47e0d6236aaddd3baacfa1c9afa8f1
Ingrid <A00826973@itesm.mx>
2020-10-29 14:59:47 -0600

    Velocidades

--- Tiro parabólico - copia.py
+++ Tiro parabólico - copia.py
@@ -11,8 +11,8 @@ def tap(x, y):
     if not inside(ball):
         ball.x = -199
         ball.y = -199
-        speed.x = (x + 200) / 25
-        speed.y = (y + 200) / 25
+        speed.x = (2*x + 200) / 25
+        speed.y = (2*y + 200) / 25
 
 def inside(xy):
     "Return True if xy within screen."
@@ -59,7 +59,7 @@ def move():
         if not inside(target):
             return
 
-    ontimer(move, 50)
+    ontimer(move, 25)
 
 setup(420, 420, 370, 0)
 hideturtle()
